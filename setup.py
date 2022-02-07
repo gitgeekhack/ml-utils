2951b656ce97411ba6b76a901f8c70f0fe0e814e
gitgeekhack <64969815+gitgeekhack@users.noreply.github.com>
2022-02-07 11:10:16 +0530

    updated version in setup

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='mlutils',
-    version='0.0.1',
+    version='0.0.2',
     description="ML utils library for python",
     long_description_content_type="text/markdown",
     url="https://github.com/siddharth2395/mlutils",
